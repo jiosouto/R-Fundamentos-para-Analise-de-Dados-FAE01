5bc3692d57a3fdc619a6d09effc60381b5926986
Jefferson Souto <inayan@outlook.com>
2023-02-02 20:09:34 -0300

    Fundamentos da Linguagem R

--- Capitulo_01/02_Operadores.R
+++ Capitulo_01/02_Operadores.R
@@ -9,28 +9,36 @@ getwd()
 # Operadores Básicos
 
 # Soma
-x = 7 + 7
-print(x)
+x = 7 + 7       ;print(x)
 
 # Subtração
-x = 7 - 4
-print(x)
+x = 7 - 4       ;print(x)
 
 # Multiplicação
-x = 5 * 5
-print(x)
+x = 5 * 5       ;print(x)
 
 # Divisão
-x = 6 / 6
-print(x)
+x = 6 / 6       ;print(x)
 
 # Potência
-x = 3^2
-print(x)
+x = 3^2         ;print(x)
 
-x = 3**2
-print(x)
+x = 3**2        ;print(x)
 
 # Módulo
-x = 16 %% 3 
-print(x)
+x = 16 %% 3     ;print(x)
+
+
+# Operadores Relacionais
+
+# Atribuindo variáveis
+x = 7
+y = 5
+
+# Operadores
+print(x > 8)
+print(x < 8)
+print(x <= 8)
+print(x >= 8)
+print(x == 8)
+print(x != 8)
