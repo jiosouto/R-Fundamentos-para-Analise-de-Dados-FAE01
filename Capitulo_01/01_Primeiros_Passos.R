27c9ded91ead8ac8c4f07a83c00d92d27931b080
Jefferson Souto <inayan@outlook.com>
2023-02-02 19:40:38 -0300

    Fundamentos da Linguagem R

---
+++ Capitulo_01/01_Primeiros_Passos.R
@@ -0,0 +1,30 @@
+# Primeiros Passos na Linguagem R
+
+# Obs: Caso tenha problemas com a acentuação, consulte este link:
+# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+setwd("C:/FCD/BigDataRAzure/Cap02")
+getwd()
+
+
+# Nome dos Contributors
+contributors()
+
+
+# Licença
+license()
+
+
+# Informações sobre a sessão
+sessionInfo()
+
+
+# Imprimir na tela
+print('Estou iniciando minha caminhada na carreira de Cientista de Dados')
+
+
+# Criar gráficos
+plot(1:25)
