64c276f2efe7b603409ce1ed17526803ad3e4047
Jefferson Souto <inayan@outlook.com>
2023-02-02 20:06:05 -0300

    Fundamentos da Linguagem R

---
+++ Capitulo_01/02_Operadores.R
@@ -0,0 +1,36 @@
+# Operadores Básicos, Relacionais e Lógicos em R
+
+# Configurando o diretório de trabalho
+# Colocar entre aspas o diretório de trabalho que será utilizado
+# Não usar diretórios com espaço no nome
+setwd("C:/DSA/BigDataRAzure/Cap01")
+getwd()
+
+# Operadores Básicos
+
+# Soma
+x = 7 + 7
+print(x)
+
+# Subtração
+x = 7 - 4
+print(x)
+
+# Multiplicação
+x = 5 * 5
+print(x)
+
+# Divisão
+x = 6 / 6
+print(x)
+
+# Potência
+x = 3^2
+print(x)
+
+x = 3**2
+print(x)
+
+# Módulo
+x = 16 %% 3 
+print(x)
