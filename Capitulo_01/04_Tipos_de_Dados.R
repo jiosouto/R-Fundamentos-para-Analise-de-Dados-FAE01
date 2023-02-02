a24afbac74e25f44ff267b4254e34fa5cf127f3f
Jefferson Souto <inayan@outlook.com>
2023-02-02 20:37:48 -0300

    Fundamentos da Linguagem R

--- Capitulo_01/04_Tipos_de_Dados.R
+++ Capitulo_01/04_Tipos_de_Dados.R
@@ -18,3 +18,20 @@ num2 = 16.82
 num2
 mode(num2)
 typeof(num2)
+
+
+# Integer 
+# Convertemos tipos numeric para integer
+is.integer(num2)
+y = as.integer(num2)
+y
+class(y)
+mode(y)
+typeof(y)
+
+as.integer('3.17')
+as.integer("Joe")
+as.integer('Joe')
+as.integer(TRUE)
+as.integer(FALSE)
+as.integer('TRUE')
