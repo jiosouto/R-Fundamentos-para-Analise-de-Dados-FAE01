94e4a24708d2f46a88b191488e403c18a80955b0
Jefferson Souto <inayan@outlook.com>
2023-02-02 20:26:20 -0300

    Fundamentos da Linguagem R

---
+++ Capitulo_01/03_Variaveis.R
@@ -0,0 +1,14 @@
+# Variáveis em R
+
+# Configurando o diretório de trabalho
+# Colocar entre aspas o diretório de trabalho que será utilizado
+# Não usar diretórios com espaço no nome
+setwd("C:/DSA/BigDataRAzure/Cap01")
+getwd()
+
+# Criando Variáveis
+var1 = 100
+var1
+mode(var1)
+help("mode")
+sqrt(var1)
