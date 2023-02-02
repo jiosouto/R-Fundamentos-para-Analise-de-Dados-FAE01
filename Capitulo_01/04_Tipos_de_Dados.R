419c633d9a24397963d751261ec15fe346da71e0
Jefferson Souto <inayan@outlook.com>
2023-02-02 20:37:24 -0300

    Fundamentos da Linguagem R

---
+++ Capitulo_01/04_Tipos_de_Dados.R
@@ -0,0 +1,20 @@
+# Tipos Básicos de Dados em R
+
+# Configurando o diretório de trabalho
+# Colocar entre aspas o diretório de trabalho que será utilizado
+# Não usar diretórios com espaço no nome
+setwd("C:/DSA/BigDataRAzure/Cap01")
+getwd()
+
+# Numeric - Todos os números criados em R são do modo numeric
+# São armazenados como números decimais (double)
+num1 <- 7
+num1
+class(num1)
+mode(num1)
+typeof(num1)
+
+num2 = 16.82
+num2
+mode(num2)
+typeof(num2)
