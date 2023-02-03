4d50aacb0f0660cb51def827f3e38d14ac91011f
Jefferson Souto <inayan@outlook.com>
2023-02-02 22:03:30 -0300

    Fundamentos da Linguagem R

---
+++ Capitulo_01/05_Tipos_Dados_Hard.R
@@ -0,0 +1,18 @@
+# Tipos Avançados de Dados em R
+
+# Configurando o diretório de trabalho
+# Colocar entre aspas o diretório de trabalho que será utilizado
+# Não usar diretórios com espaço no nome
+setwd("C:/DSA/BigDataRAzure/Cap01")
+getwd()
+
+# Vetor: possui 1 dimensão e 1 tipo de dado
+
+vetor1 <- c(1:20)
+vetor1
+length(vetor1)
+mode(vetor1)
+class(vetor1)
+typeof(vetor1)
+
+
