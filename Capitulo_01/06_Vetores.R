f7ae876a212594ef3fec2a7470121ae01d4c19c5
Jefferson Souto <inayan@outlook.com>
2023-02-03 00:02:56 -0300

    Fundamentos da Linguagem R

---
+++ Capitulo_01/06_Vetores.R
@@ -0,0 +1,11 @@
+# Vetores, Operações com Vetores e Vetores Nomeados
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+setwd("C:/DSA/BigDataRAzure/Cap01")
+getwd()
+
+# Vetor de strings
+vetor_caracter = c("Data", "Science", "Academy")
+vetor_caracter
