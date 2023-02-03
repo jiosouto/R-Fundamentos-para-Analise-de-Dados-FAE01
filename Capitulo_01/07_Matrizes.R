ed7e7c09353ae3846a2b48fa749dbd9d50c3b749
Jefferson Souto <inayan@outlook.com>
2023-02-03 00:12:34 -0300

    Fundamentos da Linguagem R

---
+++ Capitulo_01/07_Matrizes.R
@@ -0,0 +1,14 @@
+# Matrizes, Operações com Matrizes e Matrizes Nomeados
+
+# Configurando o diretório de trabalho
+# Colocar entre aspas o diretório de trabalho que você utilizado
+# Não usar diretórios com espaço no nome
+setwd("C:/DSA/BigDataRAzure/Cap01")
+getwd()
+
+# Criando Matrizes
+
+# Número de Linhas
+matrix (c(1,2,3,4,5,6), nr = 2)
+matrix (c(1,2,3,4,5,6), nr = 3)
+matrix (c(1,2,3,4,5,6), nr = 6)
