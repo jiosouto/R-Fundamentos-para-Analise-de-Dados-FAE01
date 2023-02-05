74130c0bd5e9e2bf0579dcb368a3108eab1c1d31
Jefferson Souto <inayan@outlook.com>
2023-02-04 23:49:11 -0300

    Fundamentos da Linguagem R

---
+++ Capitulo_01/08_Listas.R
@@ -0,0 +1,19 @@
+# Listas, Operações com Listas e Listas Nomeadas
+
+# Configurando o diretório de trabalho
+# Colocar entre aspas o diretório de trabalho que você utilizado
+# Não usar diretórios com espaço no nome
+setwd("C:/DSA/BigDataRAzure/Cap01")
+getwd()
+
+# Use list() para criar listas
+
+# Lista de strings
+lista_caracter1 = list('A', 'B', 'C')
+lista_caracter1
+lista_caracter2 = list(c("A", "A"), 'B', 'C')
+lista_caracter2
+lista_caracter3 = list(matrix(c("A", "A", "A", "A"), nr = 2), 'B', 'C')
+lista_caracter3
+
+
