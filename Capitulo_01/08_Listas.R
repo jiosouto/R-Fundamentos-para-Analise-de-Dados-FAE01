f885ca510c4da783aa51b463bf06e0e5913e0337
Jefferson Souto <inayan@outlook.com>
2023-02-04 23:52:23 -0300

    Fundamentos da Linguagem R

--- Capitulo_01/08_Listas.R
+++ Capitulo_01/08_Listas.R
@@ -17,3 +17,32 @@ lista_caracter3 = list(matrix(c("A", "A", "A", "A"), nr = 2), 'B', 'C')
 lista_caracter3
 
 
+# Lista de números inteiros
+lista_inteiros = list(2, 3, 4)
+lista_inteiros
+
+
+# Lista de floats
+lista_numerico = list(1.90, 45.3, 300.5)
+lista_numerico
+
+
+# Lista de números complexos
+lista_complexos = list(5.2+3i, 2.4+8i)
+lista_complexos
+
+
+# Lista de valores lógicos
+lista_logicos = list(TRUE, FALSE, FALSE)
+lista_logicos
+
+
+# Listas Compostas
+lista_composta1 = list("A", 3, TRUE)
+lista_composta1
+
+lista1 <- list(1:10, c("Zico", "Ronaldo", "Garrincha"), rnorm(10))
+lista1
+
+?rnorm
+
