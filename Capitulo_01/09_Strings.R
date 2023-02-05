97410b314ee2732bd6c3a692c1a363aeff2f5e1f
Jefferson Souto <inayan@outlook.com>
2023-02-05 13:17:13 -0300

    Fundamentos da Linguagem R

---
+++ Capitulo_01/09_Strings.R
@@ -0,0 +1,27 @@
+# Listas, Operações com Listas e Listas Nomeadas
+
+# Configurando o diretório de trabalho
+# Colocar entre aspas o diretório de trabalho que você utilizado
+# Não usar diretórios com espaço no nome
+setwd("C:/DSA/BigDataRAzure/Cap01")
+getwd()
+
+# String
+texto <- "Isso é uma string!"
+texto
+
+x = as.character(3.14) 
+x
+class(x) 
+
+
+# Concatenando Strings
+nome = "Nelson"; sobrenome = "Mandela" 
+paste(nome, sobrenome)
+cat(nome, sobrenome)
+
+
+# Formatando a saída
+sprintf("A %s é nota %d", "Data Science Academy", 10) 
+
+
