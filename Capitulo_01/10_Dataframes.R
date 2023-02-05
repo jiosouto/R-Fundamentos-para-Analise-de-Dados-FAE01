8af93ff844f99a07b88548a51d29de67219b8d7e
Jefferson Souto <inayan@outlook.com>
2023-02-05 13:24:47 -0300

    Fundamentos da Linguagem R

--- Capitulo_01/10_Dataframes.R
+++ Capitulo_01/10_Dataframes.R
@@ -33,3 +33,45 @@ codigo = c(5001, 2183, 4702, 7965, 8890)
 pesquisa = data.frame(pais, nome, altura, codigo)
 pesquisa
 
+
+# Adicionando um novo vetor a um dataframe existente
+olhos = c("verde", "azul", "azul", "castanho", "castanho")
+pesq = cbind(pesquisa, olhos)
+pesq
+
+
+# Informações sobre o dataframe
+str(pesq)
+dim(pesq)
+length(pesq)
+
+
+# Obtendo um vetor de um dataframe
+pesq$pais
+pesq$nome
+
+
+# Extraindo um único valor
+pesq[1,1]
+pesq[3,2]
+
+
+# Número de Linhas e Colunas
+nrow(pesq)
+ncol(pesq)
+
+# Primeiros elementos do dataframe
+head(pesq)
+head(mtcars)
+
+
+# Últimos elementos do dataframe
+tail(pesq)
+tail(mtcars)
+
+
+# Data frames built-in do R
+?mtcars
+mtcars
+View(mtcars)
+
