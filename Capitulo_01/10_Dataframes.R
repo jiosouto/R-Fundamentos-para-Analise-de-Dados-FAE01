37d3ffc087f1cdee97d579778a04231a613680eb
Jefferson Souto <inayan@outlook.com>
2023-02-05 13:25:15 -0300

    Fundamentos da Linguagem R

--- Capitulo_01/10_Dataframes.R
+++ Capitulo_01/10_Dataframes.R
@@ -75,3 +75,17 @@ tail(mtcars)
 mtcars
 View(mtcars)
 
+# Filtro para um subset de dados que atendem a um critério
+pesq[altura < 1.60,]
+pesq[altura < 1.60, c('codigo', 'olhos')]
+pesq
+
+
+# Dataframes Nomeados
+names(pesq) <- c("País", "Nome", "Altura", "Código", "Olhos")
+pesq
+
+colnames(pesq) <- c("Var 1", "Var 2", "Var 3", "Var 4", "Var 5")
+rownames(pesq) <- c("Obs 1", "Obs 2", "Obs 3", "Obs 4", "Obs 5")
+pesq
+
