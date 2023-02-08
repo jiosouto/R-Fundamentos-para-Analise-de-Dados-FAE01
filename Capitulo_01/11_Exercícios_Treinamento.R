cda1591ae17551a71e5c7100e838dcb176068e82
Jefferson Souto <inayan@outlook.com>
2023-02-07 23:16:01 -0300

    Fundamentos da Linguagem R

--- Capitulo_01/11_Exercícios_Treinamento.R
+++ Capitulo_01/11_Exercícios_Treinamento.R
@@ -48,3 +48,7 @@ View(iris)
 
 # Exercício 7 - Crie um plot simples usando as duas primeiras colunas do dataframe iris
 plot(iris$Sepal.Length, iris$Sepal.Width)
+
+
+# Exercício 8 - Usando s função subset, crie um novo dataframe com o conjunto de dados do dataframe iris em que Sepal.Length > 7
+# Dica: consulte o help para aprender como usar a função subset()
